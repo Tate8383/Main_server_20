0c609d88984dcd4e06686cf7ab0db7c3c7865f60
SkyHavenRP <skyhavenrp@gmail.com>
2022-03-15 08:36:38 +0000

    Voice and admin,bob,connect and menu

---
+++ resources/[voice]/qb-radio/client.lua
@@ -0,0 +1,135 @@
+local NADRP = exports['NADRP-core']:GetCoreObject()
+local radioMenu = false
+local isLoggedIn = false
+local onRadio = false
+local RadioChannel = 0
+
+RegisterNetEvent('NADRP:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+end)
+
+RegisterNetEvent('NADRP:Client:OnPlayerUnload', function()
+    isLoggedIn = false
+    leaveradio()
+end)
+
+RegisterNetEvent('qb-radio:use', function()
+    toggleRadio(not radioMenu)
+end)
+
+RegisterNetEvent('qb-radio:onRadioDrop', function()
+    if RadioChannel ~= 0 then
+        leaveradio()
+    end
+end)
+
+-- Main Thread
+
+CreateThread(function()
+    while true do
+        Wait(1000)
+        if isLoggedIn and onRadio then
+            NADRP.Functions.TriggerCallback('qb-radio:server:GetItem', function(hasItem)
+                if not hasItem then
+                    if RadioChannel ~= 0 then
+                        leaveradio()
+                    end
+                end
+            end,"radio")
+        end
+    end
+end)
+
+-- Functions
+
+function connecttoradio(channel)
+    RadioChannel = channel
+    if onRadio then
+        exports["pma-voice"]:setRadioChannel(0)
+    else
+        onRadio = true
+        exports["pma-voice"]:setVoiceProperty("radioEnabled", true)
+    end
+    exports["pma-voice"]:setRadioChannel(channel)
+    if SplitStr(tostring(channel), ".")[2] ~= nil and SplitStr(tostring(channel), ".")[2] ~= "" then
+        NADRP.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. ' MHz', 'success')
+    else
+        NADRP.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. '.00 MHz', 'success')
+    end
+end
+
+function leaveradio()
+    RadioChannel = 0
+    onRadio = false
+    exports["pma-voice"]:setRadioChannel(0)
+    exports["pma-voice"]:setVoiceProperty("radioEnabled", false)
+    NADRP.Functions.Notify(Config.messages['you_leave'] , 'error')
+end
+
+function SplitStr(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+function IsRadioOn()
+    return onRadio
+end
+
+exports("IsRadioOn", IsRadioOn)
+
+-- NUI
+RegisterNUICallback('joinRadio', function(data, cb)
+    local rchannel = tonumber(data.channel)
+    if rchannel ~= nil then
+        if rchannel <= Config.MaxFrequency and rchannel ~= 0 then
+            if rchannel ~= RadioChannel then
+                if Config.RestrictedChannels[rchannel] ~= nil then
+                    local xPlayer = NADRP.Functions.GetPlayerData()
+                    if Config.RestrictedChannels[rchannel][xPlayer.job.name] and xPlayer.job.onduty then
+                        connecttoradio(rchannel)
+                    else
+                        NADRP.Functions.Notify(Config.messages['restricted_channel_error'], 'error')
+                    end
+                else
+                    connecttoradio(rchannel)
+                end
+            else
+                NADRP.Functions.Notify(Config.messages['you_on_radio'] , 'error')
+            end
+        else
+            NADRP.Functions.Notify(Config.messages['invalid_radio'] , 'error')
+        end
+    else
+        NADRP.Functions.Notify(Config.messages['invalid_radio'] , 'error')
+    end
+end)
+
+function toggleRadio(toggle)
+    radioMenu = toggle
+    SetNuiFocus(radioMenu, radioMenu)
+    if radioMenu then
+        PhonePlayIn()
+        SendNUIMessage({type = "open"})
+    else
+        PhonePlayOut()
+        SendNUIMessage({type = "close"})
+    end
+end
+
+RegisterNUICallback('leaveRadio', function(data, cb)
+    if RadioChannel == 0 then
+        NADRP.Functions.Notify(Config.messages['not_on_radio'], 'error')
+    else
+        leaveradio()
+    end
+end)
+
+RegisterNUICallback('escape', function(data, cb)
+    toggleRadio(false)
+end)
---
+++ resources/[voice]/qb-radio/server.lua
@@ -0,0 +1,28 @@
+local NADRP = exports['NADRP-core']:GetCoreObject()
+
+NADRP.Functions.CreateUseableItem("radio", function(source, item)
+    TriggerClientEvent('qb-radio:use', source)
+end)
+
+NADRP.Functions.CreateCallback('qb-radio:server:GetItem', function(source, cb, item)
+    local src = source
+    local Player = NADRP.Functions.GetPlayer(src)
+    if Player ~= nil then
+        local RadioItem = Player.Functions.GetItemByName(item)
+        if RadioItem ~= nil and not Player.PlayerData.metadata["isdead"] and
+            not Player.PlayerData.metadata["inlaststand"] then
+            cb(true)
+        else
+            cb(false)
+        end
+    else
+        cb(false)
+    end
+end)
+
+for channel, config in pairs(Config.RestrictedChannels) do
+    exports['pma-voice']:addChannelCheck(channel, function(source)
+        local Player = NADRP.Functions.GetPlayer(source)
+        return config[Player.PlayerData.job.name] and Player.PlayerData.job.onduty
+    end)
+end
