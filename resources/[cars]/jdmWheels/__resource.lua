412cb1281a9bbed6f9837dbda4848164221a5298
SkyHavenRP <skyhavenrp@gmail.com>
2022-03-15 09:25:19 +0000

    jdmWheels rmodrover supra

---
+++ resources/[cars]/jdmWheels/__resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'data/carcols.meta',
+ 	'data/jdmrimscontentunlocks.meta'
+}
+
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/jdmrimscontentunlocks.meta'
+client_script "@Badger-Anticheat/acloader.lua"
